bf6360ade4513b8a6b03b2b137b0125a17b840ed
Christian Gellweiler <cgellweiler@gmail.com>
2016-08-29 20:41:12 +0200

    Fix flake8 warnings

--- setup.py
+++ setup.py
@@ -2,22 +2,22 @@ from setuptools import setup, find_packages
 from evetide import __version__
 
 setup(
-    name = 'evetide',
-    version = __version__,
-    packages = find_packages(),
-    author = 'Christian Gellweiler',
-    author_email = 'cgellweiler@gmail.com',
-    url = 'https://github.com/gelli/evetide',
-    entry_points = {
+    name='evetide',
+    version=__version__,
+    packages=find_packages(),
+    author='Christian Gellweiler',
+    author_email='cgellweiler@gmail.com',
+    url='https://github.com/gelli/evetide',
+    entry_points={
         'console_scripts': [
             'evetide = evetide.main:main',
         ],
     },
-    install_requires = [
+    install_requires=[
     ],
-#    package_data = {
-#        'river': [
-#            'templates/*',
-#        ],
-#    },
+    #    package_data = {
+    #        'river': [
+    #            'templates/*',
+    #        ],
+    #    },
 )
